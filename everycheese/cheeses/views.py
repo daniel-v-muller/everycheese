74a8fee39a8e9b47d059a6225d734e29893d4422
daniel.v.muller@gmail.com <daniel.muller@health.ny.gov>
2020-08-23 16:56:58 -0400

    Tracking and displaying creator of cheese records

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -18,5 +18,10 @@ class CheeseCreateView(LoginRequiredMixin,CreateView):
         'firmness',
         'country_of_origin',
     ]
+    
+    def form_valid(self,form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
 
 
